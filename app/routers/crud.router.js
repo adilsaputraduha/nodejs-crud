d8e9219d3beb06ccbea6f406edabdffd7ac0b7ee
adilsanak <adilsaputra0101@gmail.com>
2021-03-06 18:30:56 +0700

    final commit

--- app/routers/crud.router.js
+++ app/routers/crud.router.js
@@ -1,21 +1,19 @@
-const router = require('express').Router();
-const {
-    mahasiswa
-} = require('../controllers');
+const router = require("express").Router();
+const { mahasiswa } = require("../controllers");
 
 // Ambil data semua mahasiswa
-router.get('/mahasiswa', mahasiswa.getDataMahasiswa);
+router.get("/mahasiswa", mahasiswa.getDataMahasiswa);
 
 // Ambil data semua mahasiswa berdasarkan id = 2
-router.get('/mahasiswa/:nobp', mahasiswa.getDataMahasiswaById);
+router.get("/mahasiswa/:nobp", mahasiswa.getDataMahasiswaById);
 
 // Tambah data mahasiswa ke database
-router.post('/mahasiswa/add', mahasiswa.addDataMahasiswa);
+router.post("/mahasiswa/", mahasiswa.addDataMahasiswa);
 
 // Edit data mahasiswa
-router.post('/mahasiswa/edit', mahasiswa.editDataMahasiswa);
+router.put("/mahasiswa/edit/:id", mahasiswa.editDataMahasiswa);
 
 // Delete data mahasiswa
-router.post('/mahasiswa/delete/', mahasiswa.deleteDataMahasiswa);
+router.delete("/mahasiswa/delete/:id", mahasiswa.deleteDataMahasiswa);
 
 module.exports = router;
